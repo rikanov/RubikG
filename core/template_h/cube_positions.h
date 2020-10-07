571ad869e1c71c1905672e79e6c13eb927f7467d
rikanov <robert.ikanov@gmail.com>
2020-10-07 13:47:00 +0200

    [m] brief description of the class

--- core/template_h/cube_positions.h
+++ core/template_h/cube_positions.h
@@ -1,5 +1,5 @@
 /*
- * Topology: it makes a graph to determine cublet positions in an N x N Rubics cube according to the rotation state of the cubes
+ * Cube positions: this singleton class makes a graph to determine cublet positions in an N x N Rubics cube according to the rotation state of the cubes so each node has 24 branches
  *
  * Copyright (C) 2020  Robert Ikanov <robert.ikanov@gmail.com>
  *
