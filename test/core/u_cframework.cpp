c92ba92c5c4685bcc7042fc2707a279b96b77a2e
rikanov <robert.ikanov@gmail.com>
2020-10-09 17:59:57 +0200

    [a] new test cases

--- test/core/u_cframework.cpp
+++ test/core/u_cframework.cpp
@@ -6,20 +6,20 @@ bool UnitTests::unit_CFramework() const
   bool success = true;
   head( "cube framework" );
 
-  clog_( Color::white, "Start Simplex instance..." );
+  clog_( Color::white, Color::uline, "Start Simplex instance..." );
   Simplex::instance();
-  clog( Color::uline, "Done." );
+  clog( "Done." );
 
-  clog_( Color::white, "Start 'cube positions' instances..." );
+  clog_( Color::white, Color::uline, "Start 'cube positions' instances..." );
   CPositions<2>::Instance();
   CPositions<3>::Instance();
   CPositions<4>::Instance();
   CPositions<5>::Instance();
-  clog( Color::uline, "Done." );
+  clog( "Done." );
 
-  clog_( Color::white, "Cube framework test..." );
+  clog_( Color::white, Color::uline, "Cube framework test..." );
   CFramework<5> test1, test2;
-  clog( Color::uline, "Done." );
+  clog( "Done." );
 
   NL();
   NL();
@@ -27,27 +27,43 @@ bool UnitTests::unit_CFramework() const
   clog( Color::white, "Start testing turns" );
   clog(               "-------------------" );
   NL();
-  clog( Color::white, Color::uline, "Test case 1", Color::off, ": X1, Y1" );
+  clog( Color::white, Color::uline, "Test case 1 :", Color::off, "X1, Y1" );
   test1.rot( _X, 1 );
   test1.rot( _Y, 1 );
   test1.print();
   
-  clog( Color::white, Color::uline, "Test case 2", Color::off, ": Y1, Z4" );
+  clog( Color::white, Color::uline, "Test case 2 :", Color::off, "Y1, Z4" );
   test2.rot( _Y, 1);
   test2.rot( _Z, 4);
   test2.print();
   
-  clog( Color::white, Color::uline, "Test case 3", Color::off, ": combine of T1 + T2 --> X1, Y1, Y1, Z4" );
+  clog( Color::white, Color::uline, "Test case 3 :", Color::off, "combine of T1 + T2 --> X1, Y1, Y1, Z4" );
   CFramework<5> test3( test1, test2 );
   test3.print();
   
-  clog( Color::white, Color::uline, "Test case 4", Color::off, ": inverse of T3" );
+  clog( Color::white, Color::uline, "Test case 4 :", Color::off, "inverse of T3" );
   CFramework<5> test4 = test3.inverse();
   test4.print();
   
-  clog( Color::white, Color::uline, "Test case 5", Color::off, ": combine of T3 + T4 --> solved cube" );
-  CFramework<5> test5(test3,test4);
+  clog( Color::white, Color::uline, "Test case 5 :", Color::off, "revert of T4 --> Z4, Z4, Z4, Y1, Y1, X1, X1, X1" );
+  CFramework<5> test5;
+  test5.rot( _Z, 4 );
+  test5.rot( _Z, 4 );
+  test5.rot( _Z, 4 );
+  test5.rot( _Y, 1 );
+  test5.rot( _Y, 1 );
+  test5.rot( _X, 1 );
+  test5.rot( _X, 1 );
+  test5.rot( _X, 1 );
   test5.print();
+
+  clog( Color::white, Color::uline, "Test case 6 :", Color::off, "combine of T3 + T4 --> solved cube" );
+  CFramework<5> test6(test3,test4);
+  test6.print();
+  
+  clog( Color::white, Color::uline, "Test case 7 :", Color::off, "combine of T3 + T5 --> solved cube" );
+  CFramework<5> test7(test3,test5);
+  test7.print();
   
   clog_( "Cube positions:", Color::bold, "onExit()", Color::off, ':' );
   CPositions<2>::OnExit();
