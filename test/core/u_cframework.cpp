457cd4cea80360ed2b978403fd9c8fdecea7779b
Robert Ikanov <robert.ikanov@gmail.com>
2020-10-18 10:15:55 +0200

    [i] testcases

--- test/core/u_cframework.cpp
+++ test/core/u_cframework.cpp
@@ -31,19 +31,23 @@ bool UnitTests::unit_CFramework() const
   test1.rotate( _X, 1 );
   test1.rotate( _Y, 1 );
   test1.print();
+  success &= test1.integrity();
   
   tcase( "Test case 2 :", " Y1, Z4" );
   test2.rotate( _Y, 1);
   test2.rotate( _Z, 4);
   test2.print();
+  success &= test2.integrity();
   
   tcase( "Test case 3", " combine of T1 + T2", "X1, Y1, Y1, Z4" );
   CFramework<5> test3( test1, test2 );
   test3.print();
+  success &= test3.integrity();
   
   tcase( "Test case 4", " inverse of T3" );
   CFramework<5> test4 = test3.inverse();
   test4.print();
+  success &= test4.integrity();
   
   tcase( "Test case 5", " revert of T4", "Z4, Z4, Z4, Y1, Y1, X1, X1, X1" );
   CFramework<5> test5;
@@ -56,25 +60,30 @@ bool UnitTests::unit_CFramework() const
   test5.rotate( _X, 1 );
   test5.rotate( _X, 1 );
   test5.print();
-
+  success &= test5.integrity();
+  
   tcase( "Test case 6", " combine of T3 + T4", "solved cube" );
   CFramework<5> test6(test3,test4);
   test6.print();
+  success &= test6.integrity();
+  success &= test6 == CFramework<5>(); // solved
   
   tcase( "Test case 7", " combine of T3 + T5", "solved cube" );
   CFramework<5> test7(test3,test5);
   test7.print();
+  success &= test7.integrity();
+  success &= test7 == CFramework<5>(); // solved
   
   tcase( "Test case 8", "integrity test of 4 x 4 cubes");
-  CFramework<4> test8A, test8C;
+  CFramework<4> test8A, test8B, test8C;
   const int num = 1000;
   int counter = 0;
-  for( int s = 0; s < num; ++s)
+  for ( int s = 0; s < num; ++s )
   {
     bool integrity = true;
     test8A.shuffle();
     test8C.shuffle();
-    CFramework<4> test8B ( CFramework<4>::Transform( test8A, test8C ) );
+    test8B = CFramework<4>::Transform( test8A, test8C );
     
     integrity &= test8A.integrity();
     integrity &= test8B.integrity();
